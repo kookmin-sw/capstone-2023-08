52bab5ab5d96e8dc1d4a526c342376ff0e129769
dh5473 <dh5473@naver.com>
2023-03-13 13:29:15 +0900

    Add initial models.py

--- server/model_api/models.py
+++ server/model_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class StoragePath(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    human_img_path = models.CharField(max_length=100, blank=True, default='')
+    cloth_img_path = models.CharField(max_length=100, blank=True, default='')
+    preprocessing_human_img_path = models.CharField(max_length=100, blank=True, default='')
+    preprocessing_cloth_img_path = models.CharField(max_length=100, blank=True, default='')
+    human_parsing_keypoints_path = models.CharField(max_length=100, blank=True, default='')
+
+    result_img_path = models.CharField(max_length=100, blank=True, default='')
