478f84611601f0b005d66b619f9adc867bac32b3
lucyya99 <80736490+lucyya99@users.noreply.github.com>
2023-03-21 14:15:17 +0900

    [Frontend] 카메라로 촬영하는 화면 개발 (#42)
    * 크롤링 모듈 개발 (#21)
    
    * musinsa ranking image crawler
    
    * add newline at end of the file
    
    ---------
    
    Co-authored-by: wynter <72314987+HyeonYoung-Lee@users.noreply.github.com>
    
    * 프론트 하단바 문제 해결
    
    * 메인 레이아웃 따로 분리
    
    * 카메라 화면(수동촬영) -> 저장하면 메인페이지로 이동
    
    ---------
    
    Co-authored-by: Hyeon-Young Lee <72314987+wynter122@users.noreply.github.com>
    Co-authored-by: wynter <72314987+HyeonYoung-Lee@users.noreply.github.com>

--- client/lib/screen/signup_success.dart
+++ client/lib/screen/signup_success.dart
@@ -1,24 +1,29 @@
-import 'package:flutter/material.dart';
-import '../constant/page_name.dart';
+import 'dart:async';
 
-class SignupSuccess extends StatelessWidget {
+import '../constant/page_name.dart';
+import 'package:flutter/material.dart';
+
+class SignupSuccess extends StatefulWidget {
   const SignupSuccess({Key? key}) : super(key: key);
 
   @override
+  State<SignupSuccess> createState() => _SignupSuccessState();
+}
+
+class _SignupSuccessState extends State<SignupSuccess> {
+  @override
+  void initState() {
+    Timer(const Duration(milliseconds: 1500), () {
+      Navigator.of(context).popUntil(ModalRoute.withName(HOME_SCREEN));
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text('저장 성공!'),
-        ),
-        // 1초 뒤 메인화면으로 이동하도록 변경 예정
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).popUntil((route) => route.toString() == HOME_SCREEN);
-          },
-          child: Text('메인화면으로 이동'),
-        ),
-      ],
+    return const Center(
+      child: Text('저장 성공!'),
     );
   }
 }
