b2b4373e995e2314c310110c65ac80b43c6ab430
wynter <72314987+HyeonYoung-Lee@users.noreply.github.com>
2023-03-23 00:09:13 +0900

    DB setting for use mysql

--- server/virtual_fitting_app/settings.py
+++ server/virtual_fitting_app/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'virtual_fitting_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DB_PASSWORD = os.environ["VFITTING_DB_PASSWORD"]
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'VFITTING',
+        'USER': 'root',
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
