24a2a7b6e2ba8ca66a4d6b1e74920c9ccf1a1c08
wynter <72314987+HyeonYoung-Lee@users.noreply.github.com>
2023-04-06 00:45:23 +0900

    add dips into DB api

--- server/dips/urls.py
+++ server/dips/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import AddDips
 
-urlpatterns = []
+urlpatterns = [
+    path('add', AddDips.as_view()),
+]
--- server/dips/views.py
+++ server/dips/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import json
+from django.views import View
+from django.http import JsonResponse
+from .models import Dips
+from accounts.models import User
+from musinsa_list.models import Goods
 
-# Create your views here.
+class AddDips(View):
+    def post(self, request):
+        # need user_id, goods_id
+        data = json.loads(request.body)
+        
+        Dips(
+            user = User.objects.get(user_id=data['user_id']),
+            goods = Goods.objects.get(id=data['goods_id'])
+        ).save()
+    
+        return JsonResponse({'message' : '찜 목록 추가 성공'}, status=200)
+
