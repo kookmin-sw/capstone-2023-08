48dd80fd8416a951d83dd22b250d9c8e85e02812
Hyeon-Young Lee <72314987+wynter122@users.noreply.github.com>
2023-04-26 00:22:05 +0900

    Revert "찜기능 API 개발 완료 (#97)" (#100)
    This reverts commit 16d30733008c0d451dfab00c1f0c172ca4a50032.

--- server/musinsa_list/urls.py
+++ server/musinsa_list/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import RecieveCrawlingResultView, ShowClothListView, ShowDetailView, DipsView
+from .views import RecieveCrawlingResultView, ShowClothListView, ShowDetailView
 
 urlpatterns = [
     path('send-result', RecieveCrawlingResultView.as_view()),
     path('cloth-list', ShowClothListView.as_view()),
     path('detail-page', ShowDetailView.as_view()),
-    path('dips', DipsView.as_view()),
 ]
