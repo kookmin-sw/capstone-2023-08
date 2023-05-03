4d14a86ffbbc0b3f3cffacb79c38c0d22c0e2165
Hyeon-Young Lee <72314987+wynter122@users.noreply.github.com>
2023-05-04 01:39:16 +0900

    회원가입시 id 중복체크 분리 및 에러 핸들링 추가 (#112)
    * jwt authentication sign up and sign in
    
    * make requirements.txt for server
    
    * add User serializer to send Client
    
    * seperate id duplication check and improve error handle
    
    ---------
    
    Co-authored-by: wynter <72314987+HyeonYoung-Lee@users.noreply.github.com>

--- server/virtual_fitting_app/urls.py
+++ server/virtual_fitting_app/urls.py
@@ -20,10 +20,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView,
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # create token
-    path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),      # verify token
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),   # reissue access token using refresh token
-    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # create token
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),      # verify token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   # reissue access token using refresh token
     path('goods/', include('musinsa_list.urls')),
     path('storage/', include('s3_access.urls')),
 ]
