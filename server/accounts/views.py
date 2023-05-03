6c5ac50ecaa7f00489269510e688fe1eb01ed698
Hyeon-Young Lee <72314987+wynter122@users.noreply.github.com>
2023-05-04 01:39:16 +0900

    회원가입시 id 중복체크 분리 및 에러 핸들링 추가 (#112)
    * jwt authentication sign up and sign in
    
    * make requirements.txt for server
    
    * add User serializer to send Client
    
    * seperate id duplication check and improve error handle
    
    ---------
    
    Co-authored-by: wynter <72314987+HyeonYoung-Lee@users.noreply.github.com>

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -4,39 +4,56 @@ from django.http import JsonResponse
 from django.contrib.auth.hashers import check_password
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import status
 
 from .models import User
 from .serializers import UserSerializer
 
 class SignUpView(View):
+    def get(self, request):
+        # check id already exists
+        data = json.loads(request.body)
+
+        try:
+            if User.objects.filter(user_id = data['user_id']).exists():
+                return JsonResponse({'message' : '이미 등록된 아이디입니다.'}, status=status.HTTP_205_RESET_CONTENT)
+            else:
+                return JsonResponse({'message' : '사용 가능한 아이디입니다.'}, status=status.HTTP_200_OK)
+
+        except KeyError:    
+           return JsonResponse({'error' : 'ID field is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
     def post(self, request):
         data = json.loads(request.body)
 
-        if User.objects.filter(user_id = data['user_id']).exists():
-            return JsonResponse({'messge' : '이미 등록된 아이디입니다.'}, status=400)
+        try:
+            user = User(
+                user_id = data['user_id'],
+                user_name = data['user_name'],
+                password = data['password'],
+                user_img_url = data['user_img_url']
+            )
+            user.save()
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
 
-        user = User(
-            user_id = data['user_id'],
-            user_name = data['user_name'],
-            password = data['password'],
-            user_img_url = data['user_img_url']
-        )
-        user.save()
-        token = TokenObtainPairSerializer.get_token(user)
-        refresh_token = str(token)
-        access_token = str(token.access_token)
+            res = JsonResponse({
+                'message' : '회원가입이 완료되었습니다.',
+                'jwt_token': {
+                    'access_token' : access_token,
+                    'refresh_token' : refresh_token },
+                },
+                status=status.HTTP_201_CREATED)
+            res.set_cookie('access_token', access_token, httponly=True)
+            res.set_cookie('refresh_token', refresh_token, httponly=True)
 
-        res = JsonResponse({
-            'message' : '회원가입이 완료되었습니다.',
-            'jwt_token': {
-                'access_token' : access_token,
-                'refresh_token' : refresh_token },
-            },
-            status=200)
-        res.set_cookie('access_token', access_token, httponly=True)
-        res.set_cookie('refresh_token', refresh_token, httponly=True)
-
-        return res
+            return res
+        
+        except KeyError:
+            return JsonResponse({'error' : 'all form must be filled.'}, status=status.HTTP_400_BAD_REQUEST)
+            
 
 class SignInView(View):
     def post(self, request):
@@ -52,20 +69,21 @@ class SignInView(View):
 
                 serializer = UserSerializer(user)
 
-                return JsonResponse({
-                    'messge' : f'{user.user_name}님, 로그인 성공.',
+                res =  JsonResponse({
+                    'message' : f'{user.user_name}님, 로그인 성공.',
                     'User' : serializer.data,
                     'jwt_token': {
                         'access_token' : access_token,
                         'refresh_token' : refresh_token },
                     },
-                    status=200)
-            else:
-                return JsonResponse({'message' : '비밀번호가 틀립니다. 다시한번 확인해주세요'}, status=400)
-        
-        res = JsonResponse({'message' : '등록되지 않은 아이디입니다.'}, status=400)
-        
-        res.set_cookie('access_token', access_token, httponly=True)
-        res.set_cookie('refresh_token', refresh_token, httponly=True)
+                    status=status.HTTP_200_OK)
 
-        return res
+                res.set_cookie('access_token', access_token, httponly=True)
+                res.set_cookie('refresh_token', refresh_token, httponly=True)
+
+                return res
+                
+            else:
+                return JsonResponse({'message' : '비밀번호가 틀립니다. 다시한번 확인해주세요'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return JsonResponse({'message' : '등록되지 않은 아이디입니다.'}, status=status.HTTP_400_BAD_REQUEST)
