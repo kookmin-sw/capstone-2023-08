3baaaebdf4b723d1ef23212a03dcdf780089858d
Donhyeok Kang <94889715+dh5473@users.noreply.github.com>
2023-05-19 15:38:39 +0900

    Feedback api (#143)
    * Feat feedback path
    
    * Refactor feedback API
    
    * Refactor feedback API

--- server/model_api/urls.py
+++ server/model_api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('human', views.HumanParsing.as_view()),
-    path('infer', views.Inference.as_view())
+    path('infer', views.Inference.as_view()),
+    path('feedback', views.ResultFeedback.as_view())
 ]
